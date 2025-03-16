ac92f0576884ea88913686b37095b939c48fba3b
emanuelnagima <emanuel.n.nagima@gmail.com>
2025-03-16 11:07:03 -0300

    UP

--- database/conexao.js
+++ database/conexao.js
@@ -1,6 +1,24 @@
-import mysql2 from "mysql/promise";
-export default function conectar(){
-    if(global.poolConexoes){
-        return 
+
+
+import mysql from "mysql2/promise.js";
+
+export default async function conectar() {
+    if (global.poolConexoes) {
+        return await global.poolConexoes.getConnection();
+    } else {
+        global.poolConexoes = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            database: 'backend',
+            port: 3066,
+            waitForConnections: true,
+            connectionLimit: 20,
+            maxIdle: 10, // max idle connections
+            idleTimeout: 60000, // idle connections timeout in milliseconds
+            queueLimit: 0,
+            enableKeepAlive: true,
+            keepAliveInitialDelay: 0,
+        });
+        return await global.poolConexoes.getConnection();
     }
 }
